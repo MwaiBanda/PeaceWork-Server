576a4e4649c07b78b3f7862356d7fa8fc585bcfe
Mwai Banda <mwai.banda@cbn.org>
2022-11-06 00:31:56 -0500

    Initial commit

---
+++ src/test/kotlin/com/peacework/ApplicationTest.kt
@@ -0,0 +1,41 @@
+package com.peacework
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.auth.*
+import io.ktor.util.*
+import io.ktor.server.sessions.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.callloging.*
+import org.slf4j.event.*
+import io.ktor.server.request.*
+import io.ktor.server.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import java.time.Duration
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import com.peacework.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
